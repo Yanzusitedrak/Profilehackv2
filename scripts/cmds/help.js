8892c2767097f5d0428a622b4ec12a21cd350563
Yanzusitedrak <164723620+Yanzusitedrak@users.noreply.github.com>
2024-04-08 19:26:04 +0800

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🤖 | Artificial Intelligence]";
+const doNotDelete = "[ 🤖 | 𝗞𝗬𝗟𝗘 𝗔𝗜𝗩𝟯]";
 /** 
 * @author NTKhang
 * @author: do not delete it
